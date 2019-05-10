02dfc7cffa2140dd6ed17f997135298d4fb3a9ae
Ant0wan <abarthel@student.42.fr>
2019-05-10 10:22:52 +0200

    ls

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 10:01:06 by abarthel          #+#    #+#             */
-/*   Updated: 2019/05/10 10:01:26 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/05/10 10:22:25 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	return (0);
 }
