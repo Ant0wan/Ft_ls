7051ea50fb040a12722f296864c69a621335ab6d
Ant0wan <abarthel@student.42.fr>
2019-05-24 18:29:26 +0200

    adding script to compile

--- src/compile_it.sh
+++ src/compile_it.sh
@@ -4,3 +4,4 @@ if [ $# -eq 0 ]
 		gcc -g -fsanitize=address -Wall -Wextra -Werror parser.c dlist.c formatting.c main.c -I../libft/include -I../include ../libft/libft.a -o ft_ls
 else
 	rm -rf ft_ls.dSYM && rm -rf ft_ls
+fi
