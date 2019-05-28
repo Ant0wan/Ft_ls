db6db58be07e067458d225a413b3d8a9f91bf4ee
Ant0wan <abarthel@student.42.fr>
2019-05-28 17:12:16 +0200

    go recurssion

--- src/dlist.c
+++ src/dlist.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 12:55:09 by abarthel          #+#    #+#             */
-/*   Updated: 2019/05/28 11:04:25 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/05/28 14:27:17 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,11 @@ t_dlist	*create_node(t_dlist *previous, t_dlist *next, struct dirent *s_dir)
 		new->next = next;
 		new->previous = previous;
 		new->s_dir = s_dir;
+		new->pw_name = NULL;
+		new->gr_name = NULL;
+		new->file_modif = NULL;
+		new->linkname = NULL;
+		new->pathname = NULL;
 		return (new);
 	}
 	else
@@ -53,6 +58,14 @@ void	free_entire_dlist(t_dlist *somewhere_in_dlist)
 	while (somewhere_in_dlist)
 	{
 		tmp = somewhere_in_dlist->previous;
+		if (somewhere_in_dlist->pw_name)
+			free(somewhere_in_dlist->pw_name);
+		if (somewhere_in_dlist->gr_name)
+			free(somewhere_in_dlist->gr_name);
+		if (somewhere_in_dlist->file_modif)
+			free(somewhere_in_dlist->file_modif);
+		if (somewhere_in_dlist->pathname)
+			free(somewhere_in_dlist->pathname);
 		free(somewhere_in_dlist);
 		somewhere_in_dlist = tmp;
 	}
