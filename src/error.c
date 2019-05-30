b900716f0f60ad59c581db3ad649342d0ae1f59d
Ant0wan <abarthel@student.42.fr>
2019-05-30 18:13:49 +0200

    on the go for readdir

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 15:20:28 by abarthel          #+#    #+#             */
-/*   Updated: 2019/05/30 16:13:21 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/05/30 18:12:52 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "error.h"
 #include "libft.h"
 
 void	print_error(char *prog_name, char *av)
 {
 	char	*vas_ret;
 
-	if (ft_asprintf(&vas_ret, "%s: %s", prog_name, av))
+	ft_asprintf(&vas_ret, "%s: %s", prog_name, av);
+	perror(vas_ret);
+	if (!vas_ret)
 	{
-		perror(vas_ret);
-		if (!vas_ret)
-		{
-			ft_memdel((void**)vas_ret);
-		}
+		ft_memdel((void**)vas_ret);
 	}
 }
 
 void	print_usage(char *prog_name, char c)
 {
-	ft_dprintf(STDERR_FILENO, "%s: illegal option -- %c\n"\
-			"usage: %s [-lRart] [file ...]\n", prog_name, c, prog_name);
+	if (!ft_dprintf(STDERR_FILENO, "%s: illegal option -- %c\n"\
+			"usage: %s [-lRart] [file ...]\n", prog_name, c, prog_name))
+	{
+		perror(NULL);
+	}
 }
--- src/readdir.c
+++ src/readdir.c
@@ -6,13 +6,24 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 17:05:26 by abarthel          #+#    #+#             */
-/*   Updated: 2019/05/30 17:08:20 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/05/30 18:13:22 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <dirent.h>
 #include <errno.h>
+#include <stdio.h>
 #include "dlist.h"
 #include "parser.h"
 
+int	store_readdir_output(char *prog_name, char *path, t_options *options)
+{
+	DIR		*ret_opendir;
 
+	ret_opendir = opendir(path);
+	if (!ret_opendir)
+	{
+		perrno(NULL);
+		return (ERROR);
+	}
+}
