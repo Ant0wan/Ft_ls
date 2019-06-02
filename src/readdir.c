49433e93b21fc88a579cd6f7e330c8b60539d6b9
Ant0wan <abarthel@student.42.fr>
2019-06-02 19:01:08 +0200

    push

--- src/readdir.c
+++ src/readdir.c
@@ -16,10 +16,25 @@
 #include "dlist.h"
 #include "parser.h"
 
+
+t_dlist	*create_dir_list(DIR *ret_opendir)
+{
+	struct dirent	*ret_readdir;
+
+	while ((ret_readdir = readdir(ret_opendir)))
+	{
+		// create a list with what is in the dir
+	}
+	if (!ret_readdir)
+		return (NULL);
+	else
+		return (ret_readdir);
+}
+
 int	store_readdir_output(char *prog_name, char *path, t_options *options)
 {
 	DIR		*ret_opendir;
-	struct dirent	*ret_readdir;
+	t_dlist		*dir_list;
 
 	ret_opendir = opendir(path);
 	if (!ret_opendir)
@@ -27,8 +42,7 @@ int	store_readdir_output(char *prog_name, char *path, t_options *options)
 		print_error(prog_name, path);
 		return (ERROR);
 	}
-	while ((ret_readdir = readdir(ret_opendir)))
-	{
-		// create a list with what is in the dir
-	}
+	dir_list = create_dir_list(ret_opendir);
+	if (!dir_list)
+		print_error(); // and free all lists for no leaks
 }
