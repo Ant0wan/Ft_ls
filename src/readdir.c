8a23f660b2cce743f184ef3387b1455b10fd4f36
Ant0wan <abarthel@student.42.fr>
2019-06-02 19:14:20 +0200

    push

--- src/readdir.c
+++ src/readdir.c
@@ -17,18 +17,30 @@
 #include "parser.h"
 
 
-t_dlist	*create_dir_list(DIR *ret_opendir)
+t_dlist	*create_dir_list(DIR *ret_opendir, t_options *options)
 {
 	struct dirent	*ret_readdir;
+	t_dlist		*next;
+	t_dlist		*previous;
+	t_dlist		*beg_list;
+	t_dlist		*end_list; // for the case -r
 
+	next = NULL;
+	previous = NULL;
+	beg_list = end_list;
 	while ((ret_readdir = readdir(ret_opendir)))
 	{
+		// introduire ici le ascii sort en fonction de ret_readdir->d_name
+		end_list = create_node(previous, next, ret_readdir);
+		ft_printf("%s\n", end_list->s_dir->d_name); // debugging
 		// create a list with what is in the dir
 	}
 	if (!ret_readdir)
 		return (NULL);
+	else if (options->r)
+		return (end_list);
 	else
-		return (ret_readdir);
+		return (beg_list);
 }
 
 int	store_readdir_output(char *prog_name, char *path, t_options *options)
@@ -42,7 +54,7 @@ int	store_readdir_output(char *prog_name, char *path, t_options *options)
 		print_error(prog_name, path);
 		return (ERROR);
 	}
-	dir_list = create_dir_list(ret_opendir);
+	dir_list = create_dir_list(ret_opendir, options);
 	if (!dir_list)
 		print_error(); // and free all lists for no leaks
 }
