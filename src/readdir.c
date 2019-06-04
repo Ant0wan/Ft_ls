5ed25a6ad89dc80be152cc48519b3689ba7d141d
Ant0wan <abarthel@student.42.fr>
2019-06-04 18:18:58 +0200

    go

--- src/readdir.c
+++ src/readdir.c
@@ -18,49 +18,7 @@
 #include "libft.h"
 #include "files.h"
 #include "display.h"
-
-//static int	compare(t_dlist *s1, t_dlist *s2, t_options *options)
-//{	
-//	(void)options; // will sort with time, ascii and -r reverse when needed
-//	if (!s1)
-//		return (0);
-//	else if (!s2)
-//		return (0);
-//	else if (ft_strcmp(s1->s_dir->d_name, s2->s_dir->d_name) > 0)
-////	if (ft_strcmp(s1, s2) > 0)
-//		return (1);
-//	else
-//		return (0);
-//}
-
-static t_dlist	*insert_sort(t_dlist *list, struct dirent *ret_readdir, t_options *options)
-{
-	static t_dlist	*beg_list;
-	static t_dlist	*voyager;
-
-	if (!list)
-	{
-		list = create_node(NULL, NULL, ret_readdir);
-		beg_list = list;
-		voyager = list;
-	}
-	else
-	{
-		(void)options;
-		// FAUX ici car concevoir le tri a l'insertion
-	//	while (voyager)
-	//	{
-	//		if (compare(voyager->previous, voyager, options) > 0)
-	//			voyager = voyager->next;
-	//		else
-	//			voyager = voyager->previous;
-	//	}
-		voyager->next = create_node(list, NULL, ret_readdir);
-	//	voyager = create_node(list, NULL, ret_readdir);
-		voyager = voyager->next;
-	}
-	return (beg_list);
-}
+#include "insert_sort.h"
 
 static t_dlist	*create_dir_list(DIR *ret_opendir, t_options *options)
 {
@@ -89,9 +47,8 @@ static int	store_readdir_output(char *prog_name, char *path, t_options *options)
 			return (print_error(prog_name, path));
 	}
 	dir_list = create_dir_list(ret_opendir, options);
-	if (!dir_list)
-		return (print_error(NULL, NULL));
-	ft_printf("> %s:\n", path);
+	if (options->upr)
+		ft_printf("> %s:\n", path);
 	ret_value = display_list_content(dir_list, options);
 	return (ret_value);
 }
