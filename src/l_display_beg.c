6d4d33750e8a257ab26e2e950d23be3b98cf3b38
Ant0wan <abarthel@student.42.fr>
2019-06-27 15:22:04 +0200

    go modif width -l and total

--- src/l_display_beg.c
+++ src/l_display_beg.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 17:05:26 by abarthel          #+#    #+#             */
-/*   Updated: 2019/06/25 16:46:53 by sel-ahma         ###   ########.fr       */
+/*   Updated: 2019/06/27 15:11:46 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ int				display_l1(t_dlist *list, t_options *options,
 		return (print_error(NULL, NULL));
 	}
 	ft_get_cplinfos(list, &infos, options, first);
-	if ((first == 1 && list->rights[0] == 'd' && first != 4) || !first)
+	if ((first == 1 && list->rights[0] == 'd') || !first || first != 4)
 	{
 		if (ft_printf("total %u\n", infos.total_blocks) < 0)
 		{
