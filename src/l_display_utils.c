abca310f8603c16c8f79827dd81967773beb6327
Ant0wan <abarthel@student.42.fr>
2019-06-30 17:50:31 +0200

    norme ok, please do not touch

--- src/l_display_utils.c
+++ src/l_display_utils.c
@@ -6,7 +6,7 @@
 /*   By: sel-ahma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/09 14:50:35 by sel-ahma          #+#    #+#             */
-/*   Updated: 2019/06/30 12:58:44 by sel-ahma         ###   ########.fr       */
+/*   Updated: 2019/06/30 17:49:01 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,58 +16,6 @@
 #include "dlist.h"
 #include "parser.h"
 #include "error.h"
-
-/*extern inline int	ft_print_access_date(t_dlist *list)
-{
-	char *str;
-
-	if (!(str = ctime(&list->statbuf.st_atime)))
-		return (1);
-	if (!(ft_file_time(list->statbuf.st_atime)))
-	{
-		if (ft_printf("%.12s ", &str[4]) < 0)
-		{
-			free_entire_dlist(list);
-			return (print_error(NULL, NULL));
-		}
-	}
-	else
-	{
-		if (ft_printf("%.7s%5.4s ", &str[4], &str[20]) < 0)
-		{
-			free_entire_dlist(list);
-			return (print_error(NULL, NULL));
-		}
-	}
-	return (OK);
-}
-
-extern inline int	ft_print_date(t_dlist *list, t_options options)
-{
-	char	*str;
-
-	if (options.u)
-		return (ft_print_access_date(list));
-	if (!(str = ctime(&list->statbuf.st_mtime)))
-		return (1);
-	if (!(ft_file_time(list->statbuf.st_mtime)))
-	{
-		if (ft_printf("%.12s ", &str[4]) < 0)
-		{
-			free_entire_dlist(list);
-			return (print_error(NULL, NULL));
-		}
-	}
-	else
-	{
-		if (ft_printf("%.7s%5.4s ", &str[4], &str[20]) < 0)
-		{
-			free_entire_dlist(list);
-			return (print_error(NULL, NULL));
-		}
-	}
-	return (OK);
-}*/
 
 extern inline int	ft_print_symlink(t_dlist *list)
 {
