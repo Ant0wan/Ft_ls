e585ecf16023103436cb4600206ff7cbf054dc44
Ant0wan <abarthel@student.42.fr>
2019-07-07 16:39:06 +0200

    delete attribute alloc_size

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 11:55:45 by abarthel          #+#    #+#             */
-/*   Updated: 2019/06/26 19:39:29 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/07/07 16:38:34 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,7 +151,7 @@ int				ft_strnequ(char const *s1, char const *s2, size_t n);
 
 void			*ft_memalloc(size_t size);
 
-char			*ft_strnew(size_t size) __attribute__((alloc_size(1)));
+char			*ft_strnew(size_t size);
 
 char			*ft_strmap(char const *s, char (*f)(char));
 
